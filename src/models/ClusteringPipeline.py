cca985a6470fbbb445111e1052b08de4b0c36a38
palexbg <56965360+palexbg@users.noreply.github.com>
2025-03-09 11:24:38 +0100

    some refactoring

--- src/models/ClusteringPipeline.py
+++ src/models/ClusteringPipeline.py
@@ -3,17 +3,22 @@ import numpy as np
 import pandas as pd
 import warnings
 import matplotlib.pyplot as plt
+import yaml
+
 from sklearn_extra.cluster import KMedoids
 from sklearn.metrics import pairwise_distances
 from src.models.UMAPReducer import UMAPReducer
 from src.models.ClassicalClustering import ClassicalClustering
 from src.models.QuantumClustering import QuantumClustering
+from box import ConfigBox
+
 
 warnings.filterwarnings("ignore")
+ 
+def run_pipeline(config):
 
-def run_pipeline():
-    np.random.seed(42)
-
+    np.random.seed(config.classical_clustering.random_state)
+    
     script_dir = os.path.dirname(os.path.abspath(__file__))
     data_dir = os.path.abspath(os.path.join(script_dir, "..", "..", "data"))
     output_csv = os.path.join(data_dir, "antique_train_with_embeddings.csv")
@@ -28,27 +33,32 @@ def run_pipeline():
     train_df = pd.read_csv(output_csv, converters={"doc_embedding": parse_embedding})
     doc_embeddings = np.stack(train_df["doc_embedding"].values)
 
+    # AP: This part should be optional, like a pre-clustering step, so that we can compare to full clustering
     umap_reducer = UMAPReducer()
     doc_embeddings_reduced = umap_reducer.fit_transform(doc_embeddings)
     np.save(os.path.join(data_dir, "doc_embeddings_reduced.npy"), doc_embeddings_reduced)
     umap_reducer.plot_embeddings(doc_embeddings_reduced, save_path=umap_plot_path)
-
-    clustering = ClassicalClustering()
+    
+    clustering = ClassicalClustering(**config.classical_clustering)
     kmedoid_labels, medoid_indices = clustering.find_optimal_k(doc_embeddings_reduced)
     medoid_embeddings = clustering.extract_medoids(doc_embeddings_reduced, medoid_indices)
     np.save(os.path.join(data_dir, "medoid_embeddings.npy"), medoid_embeddings)
     np.save(os.path.join(data_dir, "medoid_indices.npy"), medoid_indices)
     clustering.plot_clusters(doc_embeddings_reduced, kmedoid_labels, medoid_embeddings, save_path=kmedoids_plot_path)
-
-    quantum_clustering = QuantumClustering()
-    qubo_matrix = quantum_clustering.build_qubo_matrix(medoid_embeddings)
-    refined_medoid_indices = quantum_clustering.solve_qubo(qubo_matrix, doc_embeddings_reduced)
-
-    refined_medoid_embeddings = doc_embeddings_reduced[refined_medoid_indices]
+     
+    doc_embeddings_reduced = np.load(os.path.join(data_dir, "doc_embeddings_reduced.npy"))
+    medoid_embeddings = np.load(os.path.join(data_dir, "medoid_embeddings.npy"))
+      
+    k_range = config.quantum_clustering.k_range
+    #AP: we instantiate the object with the desired k_range and the embeddings obtained at the previous step (to fit on the QPU)
+    quantum_clustering = QuantumClustering(k_range, medoid_embeddings, config)
+    refined_medoid_indices, silhouettes_quantum = quantum_clustering.solve_qubo(doc_embeddings_reduced)
+ 
+    refined_medoid_embeddings = medoid_embeddings[refined_medoid_indices] #Medoid embeddings is on the reduced already,. this should be it.
     final_kmedoids = KMedoids(n_clusters=len(refined_medoid_indices), metric='euclidean', random_state=42, init=refined_medoid_embeddings)
     final_kmedoids.fit(doc_embeddings_reduced)
 
-    final_cluster_labels = final_kmedoids.labels_
+    final_cluster_labels = final_kmedoids.labels_ 
     np.save(os.path.join(data_dir, "final_quantum_clusters.npy"), final_cluster_labels)
     np.save(os.path.join(data_dir, "refined_medoid_embeddings.npy"), refined_medoid_embeddings)
     np.save(os.path.join(data_dir, "refined_medoid_indices.npy"), refined_medoid_indices)
@@ -61,13 +71,21 @@ def run_pipeline():
 
     plt.figure(figsize=(8, 6))
     plt.scatter(doc_embeddings_reduced[:, 0], doc_embeddings_reduced[:, 1], c=final_cluster_labels, cmap='Spectral', s=20, alpha=0.7)
-    plt.scatter(refined_medoid_embeddings[:, 0], refined_medoid_embeddings[:, 1], c='black', marker='X', s=100)
+    plt.scatter(medoid_embeddings[:, 0], medoid_embeddings[:, 1], c='green', marker='X', s=100, label='All Centroid Points in the reduced set (1st stage)')
+    plt.scatter(refined_medoid_embeddings[:, 0], refined_medoid_embeddings[:, 1], c='black', marker='X', s=100, label='Quantum K-medoids (2nd stage)')
+    plt.scatter(final_kmedoids.cluster_centers_[:, 0], final_kmedoids.cluster_centers_[:, 1], c='red', marker='X', s=100, label='Classical k-medoids solution for all points')
+    plt.legend(loc='best')
+    
     plt.title("Final Quantum Cluster Assignments")
     plt.xlabel("UMAP Dimension 1")
     plt.ylabel("UMAP Dimension 2")
     plt.savefig(final_clusters_plot_path, dpi=300)
     print(f"Saved final quantum cluster plot at: {final_clusters_plot_path}")
-    plt.show()
+ 
 
 if __name__ == "__main__":
-    run_pipeline()
+        # Load config
+    with open("config/kmedoids.yml", "r") as file:
+        config = ConfigBox(yaml.safe_load(file))
+    
+    run_pipeline(config)
